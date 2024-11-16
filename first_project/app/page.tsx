f9ff35570337ef7ece70d3b223c68641b510deea
AbulQasimbintariq <www.qasimbhalli6547@gmail.com>
2024-11-16 20:48:42 +0500

    create next

---
+++ first_project/app/page.tsx
@@ -0,0 +1,9 @@
+// import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="md:bg-red-500 bg-yellow-500 py-5">
+         Hello from Qasim
+    </div>
+  );
+}
